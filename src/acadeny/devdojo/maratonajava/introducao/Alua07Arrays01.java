d6c9d899a36d1413a7fcd4d32fd09eb9c59481d5
Emerson Ferreira <sfr.emerson@gmail.com>
2024-12-22 20:14:36 -0300

    Arrays aula 1

---
+++ src/acadeny/devdojo/maratonajava/introducao/Alua07Arrays01.java
@@ -0,0 +1,13 @@
+package acadeny.devdojo.maratonajava.introducao;
+
+public class Alua07Arrays01 {
+    public static void main(String[] args) {
+        int[] idades = new int[3];
+        idades[0] = 13;
+        idades[1] = 20;
+        idades[2] = 24;
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
