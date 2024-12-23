cb6c1d820624e4baccdbc6f03eadb682c7be4a02
Emerson Ferreira <sfr.emerson@gmail.com>
2024-12-22 21:00:04 -0300

    Arrays aula 2 e 3

---
+++ src/acadeny/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -0,0 +1,15 @@
+package acadeny.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+        // byte, short, int, long, float e double padrão é 0
+        // char padrão é '\0000' que é espaço em branco
+        //boolean padrão é false
+        //String padrão é null
+
+        boolean[] idades = new boolean[3];
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
---
+++ src/acadeny/devdojo/maratonajava/introducao/Aula07Arrays03.java
@@ -0,0 +1,15 @@
+package acadeny.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays03 {
+    public static void main(String[] args) {
+
+        String[] nomes = new String[3];
+        nomes[0] = "Emerson";
+        nomes[1] = "Memen";
+        nomes[2] = "Memkzz";
+
+        for (int i = 0; i <nomes.length; i++) {
+        System.out.println(nomes[i]);
+        }
+    }
+}
