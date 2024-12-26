c238d0bf621f4ac1f142ccaf80b26f351ba0d777
Emerson Ferreira <sfr.emerson@gmail.com>
2024-12-26 20:29:08 -0300

    numeros inversos com for

---
+++ src/acadeny/devdojo/maratonajava/introducao/ExercicioGPT05.java
@@ -0,0 +1,11 @@
+package acadeny.devdojo.maratonajava.introducao;
+
+public class ExercicioGPT05 {
+    public static void main(String[] args) {
+        int [] numeros = {1,2,3,4,5};
+
+        for (int i = numeros.length - 1; i >= 0; i--){
+            System.out.println(numeros[i]);
+        }
+    }
+}
