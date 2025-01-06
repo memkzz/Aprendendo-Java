4c002c411fd22549d853f77a028e4dd60f030842
Emerson Ferreira <sfr.emerson@gmail.com>
2025-01-06 02:02:52 -0300

    Scan

---
+++ src/acadeny/devdojo/maratonajava/javacore/Gassociation/test/Input.java
@@ -0,0 +1,13 @@
+package acadeny.devdojo.maratonajava.javacore.Gassociation.test;
+
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite algo: ");
+        String algo = input.nextLine();
+        System.out.println(algo);
+    }
+}
