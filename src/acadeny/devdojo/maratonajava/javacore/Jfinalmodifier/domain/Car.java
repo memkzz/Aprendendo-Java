722fb28d0445319b81880230a31033034a139eef
Emerson Ferreira <sfr.emerson@gmail.com>
2025-01-10 05:33:59 -0300

    Modifier final primitive tyé

---
+++ src/acadeny/devdojo/maratonajava/javacore/Jfinalmodifier/domain/Car.java
@@ -0,0 +1,7 @@
+package acadeny.devdojo.maratonajava.javacore.Jfinalmodifier.domain;
+
+public class Car {
+    private String name;
+    public static final double MAXIMUM_SPEED = 250;
+
+}
---
+++ src/acadeny/devdojo/maratonajava/javacore/Jfinalmodifier/test/CarTest.java
@@ -0,0 +1,10 @@
+package acadeny.devdojo.maratonajava.javacore.Jfinalmodifier.test;
+
+import acadeny.devdojo.maratonajava.javacore.Jfinalmodifier.domain.Car;
+
+public class CarTest {
+    public static void main(String[] args) {
+        Car car = new Car();
+
+    }
+}
