340b6c55b33a587a0781e8eb79cabb8cffe34979
Emerson Ferreira <sfr.emerson@gmail.com>
2025-01-16 00:14:31 -0300

    OOP ENUMERATE 03

--- src/acadeny/devdojo/maratonajava/javacore/Kenum/domain/ClientType.java
+++ src/acadeny/devdojo/maratonajava/javacore/Kenum/domain/ClientType.java
@@ -1,5 +1,10 @@
 package acadeny.devdojo.maratonajava.javacore.Kenum.domain;
 
 public enum ClientType {
-    NATURAL_PERSON, LEGAL_ENTITY
+    NATURAL_PERSON(1), LEGAL_ENTITY(2);
+    public final int VALUE;
+
+    ClientType (int value){
+        this.VALUE = value;
+    }
 }
